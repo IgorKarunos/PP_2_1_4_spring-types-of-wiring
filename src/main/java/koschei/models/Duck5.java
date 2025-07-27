00638881b47b6b5ad4a8107134b61bd9652065ac
Ikar336 <karunos.2015@mail.ru>
2025-07-28 03:19:22 +0700

    Koshchei is definitely dead now

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -9,8 +9,8 @@ public class Duck5 {
     private final Egg6 egg;
 
     @Autowired
-    public Duck5(Egg6 egg) {
-        this.egg = egg;
+    public Duck5(Needle7 needle) {
+        this.egg = new Egg6(needle);
     }
 
     @Override
